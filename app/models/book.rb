3858d4ecd7e39b1873815b987557fcefee893b8b
JohnnyGsy <vanstrelok@gmail.com>
2022-07-06 13:11:41 +0300

    actice admin gem added,covered with specs

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,10 @@
 class Book < ApplicationRecord
+  MIN_PRICE = 0.0
+
   has_many :author_books, dependent: :destroy
   has_many :authors, through: :author_books
   belongs_to :category
+
+  validates :title, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: MIN_PRICE }
 end
